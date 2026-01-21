1ddbadb64b406432aa8bf1bd2d7ba9dccc8d238a
hereisphil <thereisphil@gmail.com>
2026-01-20 19:32:26 -0600

    Updated galaxy controller

--- controllers/galaxy.js
+++ controllers/galaxy.js
@@ -1,32 +1,41 @@
-// Show all resources
-const index = (req, res) => {
-  // Respond with an array and 2xx status code
-  res.status(200).json([`Galaxy#index`])
-}
+const { Galaxy } = require("../models");
 
-// Show resource
-const show = (req, res) => {
-  // Respond with a single object and 2xx code
-  res.status(200).json(`Galaxy#show(:id)`)
-}
+// Show all resources
+const index = async (req, res) => {
+    const galaxies = await Galaxy.findAll();
+    res.status(200).json(galaxies);
+};
+
+// Show a single resource
+const show = async (req, res) => {
+    const galaxy = await Galaxy.findByPk(req.params.id);
+    const stars = await galaxy.getStars();
+    res.status(200).json({ galaxy, stars });
+};
 
 // Create a new resource
-const create = (req, res) => {
-  // Issue a redirect with a success 2xx code
-  res.redirect(`/galaxies`, 201)
-}
+const create = async (req, res) => {
+    const galaxy = await Galaxy.create(req.body);
+    res.status(201).json(galaxy);
+};
 
 // Update an existing resource
-const update = (req, res) => {
-  // Respond with a single resource and 2xx code
-  res.status(200).json(`/galaxies/${req.params.id}`, )
-}
+const update = async (req, res) => {
+    const galaxy = await Galaxy.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+    });
+    res.status(200).json(galaxy);
+};
 
 // Remove a single resource
-const remove = (req, res) => {
-  // Respond with a 2xx status code and bool
-  res.status(204).json(true)
-}
+const remove = async (req, res) => {
+    const result = await Galaxy.destroy({
+        where: req.params,
+    });
+    res.status(204).json(result);
+};
 
 // Export all controller actions
-module.exports = { index, show, create, update, remove }
+module.exports = { index, show, create, update, remove };
--- models/galaxy.js
+++ models/galaxy.js
@@ -1,25 +1,27 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Galaxy extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Galaxy extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            models.Galaxy.hasMany(models.Star);
+        }
     }
-  }
-  Galaxy.init({
-    name: DataTypes.STRING,
-    size: DataTypes.INTEGER,
-    description: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Galaxy',
-  });
-  return Galaxy;
+    Galaxy.init(
+        {
+            name: DataTypes.STRING,
+            size: DataTypes.INTEGER,
+            description: DataTypes.TEXT,
+        },
+        {
+            sequelize,
+            modelName: "Galaxy",
+        },
+    );
+    return Galaxy;
 };
